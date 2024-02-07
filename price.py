ac94aa1277778b42608459c68e25ea6ac8daae83
Cheapshot003 <oletietz@protonmail.com>
2024-02-07 12:47:15 +0000

    dollar sign fixed

--- price.py
+++ price.py
@@ -4,4 +4,4 @@ import requests
 def get_price():
   resp = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD')
 
-  return '${:,.2f}'.format(float(resp.json()["result"]['XXBTZUSD']['a'][0]))
+  return '{:,.2f}'.format(float(resp.json()["result"]['XXBTZUSD']['a'][0]))
